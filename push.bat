146662296acb47ffa90f798e0f5b59ca87fc9de0
Cheng-1018 <2141386947@qq.com>
2025-11-10 20:32:44 +0800

    表格式目录

--- push.bat
+++ push.bat
@@ -2,6 +2,22 @@
 set http_proxy=http://127.0.0.1:7897
 set https_proxy=http://127.0.0.1:7897
 git add .
-git commit -m "贝叶斯"
+git commit -m "表格式目录"
 git push
 pause
+
+@REM 回退到指定版本
+@REM @echo off
+@REM set http_proxy=http://127.0.0.1:7897
+@REM set https_proxy=http://127.0.0.1:7897
+
+@REM REM 确保在 main 分支
+@REM git checkout main
+
+@REM REM 强制回退到指定版本
+@REM git reset --hard 59d0c0951b8d0ed552bf0ad7386a4412f2bc401f
+
+@REM REM 强制推送到远程仓库，覆盖远程历史
+@REM git push origin main --force
+
+@REM pause
